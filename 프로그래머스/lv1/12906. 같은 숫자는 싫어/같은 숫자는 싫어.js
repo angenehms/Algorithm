100603937573c6a2e881c146958378633934e8f1
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2023-08-16 12:12:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.85 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    
+    const result = [];
+    
+    for ( let i =0; i<arr.length; i++ ) {
+        
+        if ( arr[i] !== arr[i+1] ) {
+            
+            result.push(arr[i]);
+            
+        }
+    } 
+    
+    return result;
+}
