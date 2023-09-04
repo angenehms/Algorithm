9ea1610b63ed4f5fb10080fa59279bb768ac5074
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2023-09-04 13:55:19 +0900

    [level 2] Title: 주식가격, Time: 26.17 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.js
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -1,31 +1,4 @@
 function solution(prices) {
-    
-    const result = [];
-    // let count = 0;
-    
-    for ( let i=0; i<prices.length; i++ ) {
-        
-        let count = 0;
-        
-        for ( let j = i+1; j<prices.length; j++ ) {
-            
-            // let count = 0;
-            
-            if ( prices[i] <= prices[j] ) {
-                count++;
-            } else {
-                count++;
-                break;
-            }
-            
-            // console.log(result, count, i)
-            
-        }   
-        
-        result.push(count);
-
-    }
-    
-    return result;
-    
+    var answer = [];
+    return answer;
 }
