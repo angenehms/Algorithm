1e50a65a1298fb0f985e4910355f859d406306a5
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2023-09-07 15:59:09 +0900

    [unrated] Title: 할 일 목록, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,16 @@
+function solution(todo_list, finished) {
+    
+    const result = [];
+    
+    for ( let i = 0; i < finished.length; i ++ ) {
+        
+        if ( finished[i] === false ) {
+            
+            result.push(todo_list[i])
+            
+        }
+        
+    }
+    
+    return result;
+}
