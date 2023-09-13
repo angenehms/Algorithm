dcf6c71b147b38fafd16b73bd0bed63d6a1ef78e
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2023-09-13 18:10:44 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,13 @@
+function solution(arr, idx) {
+    
+    for ( let i = idx; i<arr.length; i++ ) {
+        
+        if ( arr[i] === 1 ) {
+            return i;
+        }
+        
+    }
+    
+    return -1;
+    
+}
