71f2ae875a10b080025bf0cc1c41f97de96fcd6c
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2024-01-19 16:40:12 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const result = [];
+    
+    while ( n !== 1 ) {
+        result.push(n)
+        n%2 === 0 ? n = n/2 : n = 3*n +1
+    }
+    
+    result.push(1)
+    
+    return result
+}
