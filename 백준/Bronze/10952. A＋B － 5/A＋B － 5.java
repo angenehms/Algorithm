eeb40f7cd3d8fb6b3a1f1bdd6fe71766656d8d2b
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2024-09-07 13:09:15 +0900

    [Bronze V] Title: A+B - 5, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			if (A!=0 && B!=0) {
+				sb.append(A+B).append("\n");
+			} else {
+				break;
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
