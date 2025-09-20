e6782c41a7e8b5dd9f0c3c9d3d332f24504398ad
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2025-09-20 11:42:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 20668 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -25,15 +27,30 @@ public class Main {
 	// 어떤 두 정점 사이에 여러개의 간선이 있을 수 있음
 	// 입력으로 주어지는 간선은 양방향
 	
+	static class Node {
+		
+		int from;
+		int to;
+		int weight;
+		
+		Node(int from, int to, int weight) {
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+		
+	} // end of Node
+	
 	static int node;
 	static int line;
 	static int start;
 	
-	static int[][] graph;
 	static boolean[] visited;
+	static List<Node>[] graph;
+	
+	static StringBuilder sb = new StringBuilder();
 	
 	static Deque<Integer> q = new ArrayDeque<>();
-	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -45,14 +62,26 @@ public class Main {
 		line = Integer.parseInt(st.nextToken());
 		start = Integer.parseInt(st.nextToken());
 		
-		graph = new int[node+1][node+1];
+		// graph 초기화
+		graph = new ArrayList[node+1];
+		for(int i=1; i<=node; i++) {
+			graph[i] = new ArrayList<>();
+		}
 		
+		// graph 그리기
 		for(int i=0; i<line; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			graph[from][to]++; // 양방향이므로
-			graph[to][from]++; // 양방향이므로
+			graph[from].add(new Node(from, to, 0));
+			graph[to].add(new Node(to, from, 0));
+
+		}
+		
+		// graph 정렬하기
+		for(int i=0; i<=node; i++) {
+			if(graph[i] == null) continue;
+			graph[i].sort((a,b) -> a.to - b.to);
 		}
 		
 		visited = new boolean[node+1];
@@ -61,16 +90,19 @@ public class Main {
 		visited = new boolean[node+1];
 		bfs(start);
 		System.out.println(sb);
-
+		
 	} // end of main
 	
 	static void dfs(int start) {
-		visited[start] = true;
-		sb.append(start).append(" ");
-		for(int i=1; i<=node; i++) {
-			if(!visited[i] && graph[start][i] > 0) dfs(i);
+		if(!visited[start]) {
+			visited[start] = true;
+			sb.append(start).append(" ");
+			for(Node node : graph[start]) {
+				if(!visited[node.to]) {
+					dfs(node.to);
+				}
+			}
 		}
-		return;
 	} // end of dfs
 	
 	static void bfs(int start) {
@@ -79,15 +111,14 @@ public class Main {
 		sb.append(start).append(" ");
 		while(!q.isEmpty()) {
 			int curr = q.poll();
-			for(int i=1; i<=node; i++) {
-				if(!visited[i] && graph[curr][i] > 0) {
-					q.add(i);
-					visited[i] = true;
-					sb.append(i).append(" ");
+			for(Node node : graph[curr]) {
+				if(!visited[node.to]) {
+					q.add(node.to);
+					visited[node.to] = true;
+					sb.append(node.to).append(" ");
 				}
 			}
 		}
-		return;
 	} // end of bfs
 	
 } // end of class
