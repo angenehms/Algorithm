368038218b40e34ff553f014c070e53070c4beee
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2025-12-08 17:15:28 +0900

    [Gold V] Title: 적록색약, Time: 120 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,128 +3,127 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int N;
-	static String[][] map;
-
-	// 상하좌우
-	static int[] dr = new int[] {-1, 1, 0, 0};
-	static int[] dc = new int[] {0, 0, -1, 1};
+	static char[][] map;
 	
+	// 상하좌우
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
 	static int nr;
 	static int nc;
 	
-	static boolean[][] visited;
+	static int[][] yesMap;
+	static int[][] noMap;
 	
-	static int[][] can;
-	static int[][] canNot;
+	static int yesAreaCnt = 0; // 적록색약으로 보이는 경우
+	static int noAreaCnt = 0; // 적록색약이 아닌 경우
 	
+	static Deque<Node> q;
+	
+	static class Node {
+		int r;
+		int c;
+		Node(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	} // end of Node
+
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		// map 그리기
 		N = Integer.parseInt(br.readLine());
-		map = new String[N][N];
+		// map 초기화 및 그리기
+		map = new char[N][N];
 		for(int r=0; r<N; r++) {
-			String str = br.readLine();
+			String line = br.readLine();
 			for(int c=0; c<N; c++) {
-				map[r][c] = String.valueOf(str.charAt(c));
+				map[r][c] = line.charAt(c);
 			}
 		}
 		
-		visited = new boolean[N][N];
-		Deque<int[]> q = new ArrayDeque<>();
-		can = new int[N][N];
-		canBfs(q);
-		sb.append(getMax(can)).append(" ");
+		yesMap = new int[N][N];
+		noMap = new int[N][N];
 		
-		visited = new boolean[N][N];
-		q = new ArrayDeque<>();
-		canNot = new int[N][N];
-		canNotBfs(q);
-		sb.append(getMax(canNot));
+		// 적록 색약인 경우
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<N; c++) {
+				yesBfs(r,c);
+				noBfs(r,c);
+			}
+		}
 		
+		sb.append(noAreaCnt).append(" ").append(yesAreaCnt);
 		System.out.println(sb);
 		
 	} // end of main
 	
-	static void canBfs(Deque<int[]> q) {
-		int cnt = 0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(!visited[r][c]) { // 방문한적이 없으면
-					cnt++;
-					q.add(new int[] {r,c});
-					visited[r][c] = true;
-					can[r][c] = cnt;
-					while(!q.isEmpty()) {
-						int[] curr = q.poll();
-						for(int i=0; i<4; i++) {
-							nr = curr[0] + dr[i];
-							nc = curr[1] + dc[i];
-							if(!checker()) continue;
-							if(!visited[nr][nc] && map[nr][nc].equals(map[r][c])) {
-								q.add(new int[] {nr, nc});
-								visited[nr][nc] = true;
-								can[nr][nc] = cnt;
-							}
-						}
+	static void yesBfs(int r, int c) {
+		
+		if(yesMap[r][c] != 0) return;
+		char color = map[r][c];
+		q = new ArrayDeque<>();
+		q.add(new Node(r,c));
+		yesMap[r][c] = ++yesAreaCnt;
+		
+		while(!q.isEmpty()) {
+			Node curr = q.poll();
+			for(int i=0; i<4; i++) {
+				nr = curr.r + dr[i];
+				nc = curr.c + dc[i];
+				if(!checker()) continue;
+				if(yesMap[nr][nc] != 0) continue;
+				char temp = map[nr][nc];
+				if((color == 'R' || color == 'G') && (temp == 'R' || temp == 'G')) { // color가 적록일떄, temp도 적록이면
+					q.add(new Node(nr,nc));
+					yesMap[nr][nc] = yesAreaCnt;
+				} else if(color == 'B') { // color가 블루면
+					if(temp == color) {
+						yesMap[nr][nc] = yesAreaCnt;
+						q.add(new Node(nr,nc));
 					}
+					
 				}
 			}
 		}
-	} // end of canBfs
+		
+		
+		
+	} // end of yesBfs
 	
-	static void canNotBfs(Deque<int[]> q) {
-		int cnt = 0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(!visited[r][c]) { // 방문한적이 없으면
-					cnt++;
-					q.add(new int[] {r,c});
-					visited[r][c] = true;
-					canNot[r][c] = cnt;
-					while(!q.isEmpty()) {
-						int[] curr = q.poll();
-						for(int i=0; i<4; i++) {
-							nr = curr[0] + dr[i];
-							nc = curr[1] + dc[i];
-							if(!checker()) continue;
-							if(!visited[nr][nc]) {
-								if((map[r][c].equals("R") || map[r][c].equals("G")) && (map[nr][nc].equals("R") || map[nr][nc].equals("G"))) {
-									q.add(new int[] {nr, nc});
-									visited[nr][nc] = true;
-									canNot[nr][nc] = cnt;
-								} else if(map[r][c].equals("B") && map[nr][nc].equals("B")) {
-									q.add(new int[] {nr, nc});
-									visited[nr][nc] = true;
-									canNot[nr][nc] = cnt;
-								}
-							}
-						}
-					}
+	static void noBfs(int r, int c) {
+		
+		if(noMap[r][c] != 0) return;
+		char color = map[r][c];
+		q = new ArrayDeque<>();
+		q.add(new Node(r,c));
+		noMap[r][c] = ++noAreaCnt;
+		
+		while(!q.isEmpty()) {
+			Node curr = q.poll();
+			for(int i=0; i<4; i++) {
+				nr = curr.r + dr[i];
+				nc = curr.c + dc[i];
+				if(!checker()) continue;
+				if(noMap[nr][nc] != 0) continue;
+				if(map[nr][nc] == color) {
+					q.add(new Node(nr,nc));
+					noMap[nr][nc] = noAreaCnt;
 				}
 			}
 		}
-	} // end of canNotBfs
-	
-	static int getMax(int[][] map) {
-		int max = Integer.MIN_VALUE;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(max < map[r][c]) {
-					max = map[r][c];
-				}
-			}
-		}
-		return max;
-	} // end of getMax
+		
+	} // end of noBfs
 	
 	static boolean checker() {
-		return 0<=nr && nr<N && 0<=nc && nc<N;
+		return 0<=nr && nr<N && 0<= nc && nc<N;
 	}
 	
 } // end of class
