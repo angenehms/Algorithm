54446125637144a3b93c055b685218f81fdcb36c
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2025-12-10 16:13:42 +0900

    [Gold IV] Title: 연구소, Time: 524 ms, Memory: 79624 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -8,29 +8,36 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int result = 0;
 	
 	static int N;
 	static int M;
 	static int[][] map;
-	static Deque<int[]> q = new ArrayDeque<>();
+	
+	static int[][] copyMap;
 	
 	// 상하좌우
-	static int[] dr = new int[] {-1, 1, 0, 0};
-	static int[] dc = new int[] {0, 0, -1, 1};
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
 	static int nr;
 	static int nc;
 	
-	static List<int[]> empty = new ArrayList<>();
+	static class Node {
+		int r;
+		int c;
+		Node(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	} // end of Node
 	
+	static Deque<Node> q;
+	static Deque<Node> copyQ;
+	static List<Node> emptyZone;
 	static boolean[] visited;
 	
-	static int max = Integer.MIN_VALUE;
-	
 	public static void main(String[] args) throws IOException {
-		
-		//		 0은 빈 칸, 
-		//		 1은 벽, 
-		//		 2는 바이러스
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -39,81 +46,78 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
 		
-		// map 그리기와 바이러스 위치저장
+		q = new ArrayDeque<>();
+		copyQ = new ArrayDeque<>();
+		emptyZone = new ArrayList<>();
+		
+		map = new int[N][M];
 		for(int r=0; r<N; r++) {
 			st = new StringTokenizer(br.readLine());
 			for(int c=0; c<M; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-				if(map[r][c] == 2) {
-					q.add(new int[] {r,c}); // 바이러스 위치 저장
-				} else if(map[r][c] == 0) {
-					empty.add(new int[] {r,c});
-				}
+				int temp = Integer.parseInt(st.nextToken());
+				if(temp == 2) q.add(new Node(r,c));
+				if(temp == 0) emptyZone.add(new Node(r,c));
+				map[r][c] = temp;
 			}
 		}
 		
-		visited = new boolean[empty.size()];
+		visited = new boolean[emptyZone.size()];
+		
+//		copyMap = map; // 이렇게 하면 copyMap 변환 시 map 도 변함
+		
+		copyMap = new int[N][M];
 		comb(0, 0);
 		
-		System.out.println(max);
+		System.out.println(result);
 		
 	} // end of main
 	
-	static void comb(int start, int idx) {
-		
+	static void comb(int start, int idx) { // 벽 세우기 조합 (브루트포스)
 		if(idx == 3) {
-			int[][] copyMap = new int[N][M];
 			for(int r=0; r<N; r++) {
 				for(int c=0; c<M; c++) {
 					copyMap[r][c] = map[r][c];
 				}
 			}
-			Deque<int[]> copyQ = new ArrayDeque<>();
-			for(int[] virusPoint : q) {
-				copyQ.add(new int[] {virusPoint[0], virusPoint[1]});
+			for(Node node : q) {
+				copyQ.add(node);
 			}
-			bfs(copyMap, copyQ);
-			int temp = countSafe(copyMap);
-			if(max < temp) max = temp;
+			bfs(); // 바이러스 퍼트리기
+			int thisCycleSafeArea = countSafeArea(); // 안전영역 구학
+			if(result < thisCycleSafeArea) result = thisCycleSafeArea;
 			return;
 		}
 		
-		for(int i=start; i<empty.size(); i++) {
+		for(int i=start; i<emptyZone.size(); i++) {
 			if(!visited[i]) {
-				
-				// 벽 세우기
-				int r = empty.get(i)[0];
-				int c = empty.get(i)[1];
-				map[r][c] = 1;
-				
 				visited[i] = true;
+				int r = emptyZone.get(i).r;
+				int c = emptyZone.get(i).c;
+				map[r][c] = 1;
 				comb(i+1, idx+1);
-				
-				map[r][c] = 0;
 				visited[i] = false;
+				map[r][c] = 0;
 			}
 		}
-		
 	} // end of comb
 	
-	static void bfs(int[][] copyMap, Deque<int[]> copyQ) { // 바이러스 퍼트리기
+	static void bfs() { // 세균 퍼트리기
 		while(!copyQ.isEmpty()) {
-			int[] curr = copyQ.poll();
+			Node curr = copyQ.poll();
 			for(int i=0; i<4; i++) {
-				nr = curr[0] + dr[i];
-				nc = curr[1] + dc[i];
+				nr = curr.r + dr[i];
+				nc = curr.c + dc[i];
 				if(!checker()) continue;
-				if(copyMap[nr][nc] == 0) { // 옮겨갈 곳이 빈칸이면
-					copyQ.add(new int[] {nr, nc});
+				if(copyMap[nr][nc] == 0) {
+					copyQ.add(new Node(nr, nc));
 					copyMap[nr][nc] = 2;
 				}
 			}
 		}
 	} // end of bfs
 	
-	static int countSafe(int[][] copyMap) {
+	static int countSafeArea() { // 안전지역 영역 구하기
 		int result = 0;
 		for(int r=0; r<N; r++) {
 			for(int c=0; c<M; c++) {
@@ -121,10 +125,10 @@ public class Main {
 			}
 		}
 		return result;
-	} // end of countSafe
+	} // end of countSafeArea 
 	
 	static boolean checker() {
-		return 0<= nr && nr < N && 0<= nc && nc <M;
-	}
+		return 0<=nr && nr<N && 0<=nc && nc<M;
+	} // end of checker
 	
 } // end of class
