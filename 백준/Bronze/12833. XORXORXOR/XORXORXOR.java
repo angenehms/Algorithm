a5ee99fd7a0f7e58cdb5056389ca827ab0ab225f
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2026-01-15 02:55:25 +0900

    [Bronze I] Title: XORXORXOR, Time: 152 ms, Memory: 16272 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		for(int i=0; i<C; i++) {
+			A = A^B;
+		}
+		
+		System.out.println(A);
+		
+	} // end of main
+	
+} // end of class
