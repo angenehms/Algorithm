eba80e8552e65e8abd8db59c1663bfaed1fb4687
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2026-02-25 21:40:52 +0900

    [Gold V] Title: 두 용액, Time: 344 ms, Memory: 28628 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 산성 1 ~ 10억
+		// 알칼리 -1 ~ -10억
+		
+		// 두 용액 합해서 0에 가장 가까운 용액을 만들려 함
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+	
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int left = 0;
+		int right = N-1;
+		
+		int resultL = 0;
+		int resultR = 0;
+		
+		int closest = Integer.MAX_VALUE;
+		
+		while(left < right) {
+			
+			int sum = arr[left] + arr[right];
+			int temp = Math.abs(sum);
+			
+			if(closest > temp) {
+				closest = temp;
+				resultL = left;
+				resultR = right;
+			}
+			
+			if(closest == 0) break;
+		
+			if(sum>0) {
+				right--;
+			} else {
+				left++;
+			}
+			
+		}
+		
+		if(arr[resultL] < arr[resultR]) {
+			System.out.println(arr[resultL] + " " + arr[resultR]);
+		} else {
+			System.out.println(arr[resultR] + " " + arr[resultL]);
+		}
+		
+	} // end of main
+	
+} // end of class
