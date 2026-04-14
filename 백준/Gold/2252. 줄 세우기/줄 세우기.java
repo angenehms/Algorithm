a39f8a72b3cd9970008a9cf0c76e1a38c42b99fe
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2026-04-14 19:39:40 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 44076 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -8,71 +8,65 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N;
-	static int M;
-
+	
+	static int node;
+	static int line;
+	
 	static List<Integer>[] graph;
 	static int[] degree;
+	
 	static Deque<Integer> q;
-	
 	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
 
+	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		node = Integer.parseInt(st.nextToken());
+		line = Integer.parseInt(st.nextToken());
 		
-		degree = new int[N+1];
+		degree = new int[node+1];
+		q = new ArrayDeque<>();
 		
-		// 그래프 초기화
-		graph = new ArrayList[N+1];
-		for(int i=0; i<N+1; i++) {
+		graph = new ArrayList[node+1];
+		for(int i =0; i<node+1; i++) {
 			graph[i] = new ArrayList<>();
 		}
-		
-		// 그래프 그리기 
-		for(int i=0; i<M; i++) {
+
+		for(int i=0; i<line; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			// 방향 그래프
 			graph[from].add(to);
 			degree[to]++;
 		}
 		
-		q = new ArrayDeque<>();
-		for(int i=1; i<N+1; i++) {
+		for(int i=1; i<node+1; i++) {
 			if(degree[i] == 0) {
 				q.add(i);
 			}
 		}
 		
-		topologicalSort();
+		topol();
 		System.out.println(sb);
 		
-	} // end of main 
+	} // end of main
 	
-	static void topologicalSort() {
+	static void topol() {
 		
 		while(!q.isEmpty()) {
-			
 			int curr = q.poll();
 			sb.append(curr).append(" ");
-			
 			for(int next : graph[curr]) {
 				degree[next]--;
 				if(degree[next] == 0) {
 					q.add(next);
 				}
 			}
-			
 		}
 		
-	} // end of topologicalSort
+	} // end of topol
 	
 } // end of class
