81e3d1bf159c6701c1abb3e5b019087dcb71fc80
Seok Gyu <111214565+angenehms@users.noreply.github.com>
2026-04-15 21:04:06 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,6 +1,8 @@
-# -- 코드를 작성해주세요
-select DISTINCT ID, EMAIL, FIRST_NAME, LAST_NAME
-from DEVELOPERS D
-join SKILLCODES S
-on S.CATEGORY = 'Front End' and (D.SKILL_CODE & S.CODE) != 0
-order by ID
+SELECT * FROM DEVELOPERS 
+
+# # -- 코드를 작성해주세요
+# select ID, EMAIL, FIRST_NAME, LAST_NAME
+# from DEVELOPERS D
+# join SKILLCODES S
+# on S.CATEGORY = 'Front End' and (D.SKILL_CODE & S.CODE) != 0
+# order by ID
